79ab2da9aa2432f9fae1a25c315adb8f695484c1
Brian2524 <b2hinkle@gmail.com>
2020-07-17 07:06:56 -0400

    Quick implementation. Now in a working state but really needs code cleanup and better implementation for some parts.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UE4-Test-Bench
-Provides reliable way to test your multiplayer games with accurate results
+Provides reliable way to test your multiplayer games with accurate results.
+
+Todo:
+Client count currently doesn't decrement
--- UE4 Test Bench/UE4 Test Bench/MainWindow.xaml.cs
+++ UE4 Test Bench/UE4 Test Bench/MainWindow.xaml.cs
@@ -13,7 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System.ComponentModel;
 using System.IO;
+using System.Net.Sockets;
+using System.Net;
 
 namespace UE4_Test_Bench
 {
@@ -22,19 +26,23 @@ namespace UE4_Test_Bench
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        Process ServerProcess;
+        List<Process> ClientProcesses = new List<Process>();
+
+        string localIP = "";
 
         public MainWindow()
         {
             InitializeComponent();
+            localIP = GetLocalIPAddress();
+            UProjectFilePathTxtBox.Text = MySettings.Default.UProjectFilePath;
+            EngineFilePathTxtBox.Text = MySettings.Default.UnrealEngineFilePath;
+            MySettings.Default.ServerArgs = "\"" + MySettings.Default.UProjectFilePath + "\"" + " -server -nosteam";
+            MySettings.Default.ClientArgs = "\"" + MySettings.Default.UProjectFilePath + "\" " + localIP + " -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -nosteam";
+            MySettings.Default.Save();
         }
 
-        private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFile();
-        }
-
-        private void OpenFile()
+        private void OpenUProjectFileBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog FileOpener = new OpenFileDialog()
             {
@@ -45,9 +53,142 @@ namespace UE4_Test_Bench
 
             if (FileOpener.ShowDialog() == true)
             {
-                FilePathTxtBox.Text = FileOpener.FileName;
+                UProjectFilePathTxtBox.Text = FileOpener.FileName;
+
+                MySettings.Default.UProjectFilePath = UProjectFilePathTxtBox.Text;
+                MySettings.Default.Save();
+                MySettings.Default.ServerArgs = "\"" + MySettings.Default.UProjectFilePath + "\"" + " -server -nosteam";
+                MySettings.Default.ClientArgs = "\"" + MySettings.Default.UProjectFilePath + "\" " + localIP + " -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -nosteam";
             }
+        }
+        private void OpenEngineFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog FileOpener = new OpenFileDialog()
+            {
+                Title = "Select File",
+                Filter = "UnrealEngine | *.exe",
+                FileName = " "
+            };
+
+            if (FileOpener.ShowDialog() == true)
+            {
+                EngineFilePathTxtBox.Text = FileOpener.FileName;
+                MySettings.Default.UnrealEngineFilePath = EngineFilePathTxtBox.Text;
+                MySettings.Default.Save();
+                MySettings.Default.ServerArgs = "\"" + MySettings.Default.UProjectFilePath + "\"" + " -server -nosteam";
+                MySettings.Default.ClientArgs = "\"" + MySettings.Default.UProjectFilePath + "\" " + localIP + " -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -nosteam";
+            }
+        }
+
+
+        /*string serverArgs = *//*"\"" + *//*UProjectFilePathTxtBox.Text*//* + "\""*//* + " -server -log -nosteam";
+        string clientArgs = UProjectFilePathTxtBox.Text + localIP + " -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log -nosteam";*/
+
+        private void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ServerProcess == null)
+            {
+                ServerProcess = new Process();
+                ServerProcess.EnableRaisingEvents = true;
+                ServerProcess.Exited += new EventHandler(ServerProcess_Exited);
+                string g = MySettings.Default.ServerArgs;
+                ServerProcess.StartInfo = new ProcessStartInfo()
+                {
+                    CreateNoWindow = true,
+                    FileName = EngineFilePathTxtBox.Text,
+                    Arguments = MySettings.Default.ServerArgs,
+                };
+
+
+                if (ServerProcess.Start())
+                {
+                    StartServer.Background = new SolidColorBrush(Color.FromRgb(131, 251, 186));
+                }
+            }
+            else
+            {
+                ServerProcess?.Kill();
+                StartServer.Background = new SolidColorBrush(Color.FromRgb(221, 221, 221));
+            }
+        }
+
+        private void ServerProcess_Exited(object sender, System.EventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                StartServer.Background = new SolidColorBrush(Color.FromRgb(221, 221, 221));  // access button from main thread
+            });
+            
+        }
+
+
+
+
+
+
+        private void StartClient_Click(object sender, RoutedEventArgs e)
+        {
+            Process NewClient = new Process();
+            NewClient.EnableRaisingEvents = true;
+            NewClient.Exited += new EventHandler(ClientProcess_Exited);
+            NewClient.StartInfo = new ProcessStartInfo()
+            {
+                CreateNoWindow = true,
+                FileName = EngineFilePathTxtBox.Text,
+                Arguments = MySettings.Default.ClientArgs
+            };
+
+            if (NewClient.Start())
+            {
+                ClientProcesses.Add(NewClient);
+                ClientCountTxt.Text = ClientProcesses.Count().ToString();
+                
+            }
+        }
+
+        private void ClientProcess_Exited(object sender, System.EventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                /*ClientProcesses.Remove(process);*/ // Locate the process object so we can remove it from the list
+                /*ClientCountTxt.Text = (int.Parse(ClientCountTxt.Text) - 1).ToString();*/      //  Client processes count is currently faked when decrementing but shouldn't be innacturate. Quick implementation for now
+            });
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        public static string GetLocalIPAddress()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                return endPoint.Address.ToString();
+            }
+        }
+
+        private void OnApplicationEnd(object sender, CancelEventArgs e)
+        {
+            // Faster to kill clients first since server won't have to redirect all clients to their own maps before server gets killed
+            foreach (Process P in ClientProcesses)
+            {
+                P.Exited -= new EventHandler(ClientProcess_Exited);
+                P?.Kill();
+            }
+            ServerProcess.Exited -= new EventHandler(ServerProcess_Exited);
+            ServerProcess?.Kill();
+        }
+
+        
     }
 }
